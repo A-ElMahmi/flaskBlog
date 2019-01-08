8dc5a4d7c35792caa50b5eb7344c214345800dde
AbboudMahmi <elmahmi.a04@gmail.com>
2019-01-08 20:12:20 +0000

    Design Working. User are able to sign up and log in. Users are saved to a database. Registration and Log in forms are in working order, with mechanisms to create unique users. Passwords are hashed. User could view their profile on the Account page. They could change their email and password. They could also update their profile picture.

---
+++ flaskblog/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a798a02800316b0b3fbbb755d62ce7b4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
+
+from flaskblog import routes
