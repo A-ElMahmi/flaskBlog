270c6857351c0e09755386765445b2c9c68934fd
AbboudMahmi <elmahmi.a04@gmail.com>
2019-01-08 21:34:45 +0000

    Added the new Create Post route. Saved all new posts to the database. Added a new site page called create_post, to create posts for the Flask Blog. Updated the home template to display posts from the database rather than dummy data.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -4,28 +4,14 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import ResgistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import ResgistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
-
-posts = [
-	{
-		"author": "John Doe",
-		"title": "Going on Holidays!",
-		"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quasi ratione fugit quas. Eligendi nam incidunt quaerat voluptas, labore velit.",
-		"datePosted": "July 02, 2018"
-	},
-	{
-		"author": "Tony Smith",
-		"title": "Back to School",
-		"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia quasi ratione fugit quas. Eligendi nam incidunt quaerat voluptas, labore velit.",
-		"datePosted": "August 31, 2018"
-	}
-]
 
 
 @app.route("/")
 # @app.route("/home")
 def home():
+	posts = Post.query.all()
 	return render_template('home.html', posts=posts)
 
 
@@ -101,3 +87,15 @@ def account():
 		form.email.data = current_user.email
 	profile_pic = url_for('static', filename=f"profile_pics/{current_user.image_file}")
 	return render_template('account.html', title='Profile', image_file=profile_pic, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(title=form.title.data, content=form.content.data, author=current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash("You're post has been created!", 'succes')
+		return redirect(url_for('home'))
+	return render_template('create_post.html', title='New Post', form=form)
